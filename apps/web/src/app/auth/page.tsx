64d56b9bea26c8075545e6201a26c03ca3ff57f4
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2024-09-18 23:49:25 -0300

    feat: add auth page

---
+++ apps/web/src/app/auth/page.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const authFormSchema = z.object({
+  email: z
+    .string({ message: "Campo obrigatório" })
+    .email("Informe um email válido"),
+  password: z
+    .string({ message: "Campo obrigatório" })
+    .min(8, "Informe a quantidade minima de caracteres"),
+});
+
+type LoginForm = z.infer<typeof authFormSchema>;
+
+export default function AuthPage() {
+  const form = useForm<LoginForm>({
+    resolver: zodResolver(authFormSchema),
+  });
+
+  async function onSubmit(values: LoginForm) {
+    await fetch("http://localhost:3001/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...values }),
+    });
+  }
+
+  return (
+    <main className="h-screen w-screen flex items-center justify-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">Login</CardTitle>
+          <CardDescription>
+            Preencha seu email e senha para acessar sua conta
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <Form {...form}>
+            <form className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="unifit@example.com"
+                        required
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel asChild>
+                      <div className="flex items-center">
+                        <Label htmlFor="password">Senha</Label>
+                        <Link
+                          href="/forgot-password"
+                          className="ml-auto inline-block text-sm underline text-black"
+                        >
+                          Esqueceu sua senha?
+                        </Link>
+                      </div>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="malhar@123"
+                        id="password"
+                        type="password"
+                        required
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button onClick={form.handleSubmit(onSubmit)} className="w-full">
+            Entrar
+          </Button>
+          <Link
+            href="/sing-up"
+            className="w-full text-center inline-block text-sm underline text-black"
+          >
+            Não possui conta? clique aqui!
+          </Link>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
