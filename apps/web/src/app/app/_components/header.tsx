97cf77022a6c90113dc35eb323f59ab28ed7a9b1
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2024-10-28 21:03:25 -0300

    feat: add route to get user info in header dropdown

--- apps/web/src/app/app/_components/header.tsx
+++ apps/web/src/app/app/_components/header.tsx
@@ -9,15 +9,47 @@ import {
 import useAuthStore from "@/stores/auth-store";
 import { useRouter } from "next/navigation";
 import { User, Flame, Dumbbell } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface User {
+  id: number
+  email: string
+  accessLevel: string
+  name: string
+  status: string
+  ra: string
+  cellphoneNumber: string
+  profilePicture: any
+  createdAt: string
+  updatedAt: string
+}
+
+
+async function getUserInfo(token: string): Promise<User> {
+  const { data } = await axios.get("http://localhost:3001/user/6", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return data;
+}
 
 export function Header() {
   const router = useRouter();
-  const { logout } = useAuthStore();
+  const { logout, getToken } = useAuthStore();
+  const token = getToken();
 
   function logoutUser() {
     logout();
     router.push("/auth");
   }
+
+  const { data } = useQuery({
+    queryKey: ["get-user-info"],
+    queryFn: () => getUserInfo(token!),
+  });
 
   return (
     <header className="w-full flex justify-between items-center p-2 bg-primary rounded">
@@ -37,8 +69,10 @@ export function Header() {
             </Avatar>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuLabel>Nome do usuário</DropdownMenuLabel>
-            <DropdownMenuItem className="text-red-700" onClick={logoutUser}>Sair</DropdownMenuItem>
+            <DropdownMenuLabel>{data?.ra}</DropdownMenuLabel>
+            <DropdownMenuItem className="text-red-700" onClick={logoutUser}>
+              Sair
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
