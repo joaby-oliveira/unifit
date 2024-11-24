1e766e66c5d9fff1e3691e43e4f3f99a2afa2287
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2024-11-24 10:27:36 -0300

    feat: create a adm layout area

--- apps/web/src/app/adm/layout.tsx
+++ apps/web/src/app/adm/layout.tsx
@@ -1,19 +1,51 @@
 "use client";
 
+import { Header } from "@/components/header";
 import useAuthStore from "@/stores/auth-store";
+import useUserStore from "@/stores/user-store";
+import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { decode } from "jsonwebtoken";
+import api from "@/lib/api";
+import { UserInterface } from "@/types/user";
+import { JwtPayload } from "@/types/jwt";
+
+async function getUserInfo(id: number): Promise<UserInterface> {
+  const { data } = await api.get(`/user/${id}`);
+  return data;
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { typeUser } = useAuthStore();
+  const { typeUser, getToken } = useAuthStore();
+  const { setUser, user } = useUserStore();
   const router = useRouter();
+  const token = getToken();
+  const jwtDecoded = decode(token!) as JwtPayload | null;
 
   if (typeUser === "USER") {
     router.push("/app");
   }
 
-  return children;
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["get-user-info"],
+    queryFn: () => getUserInfo(jwtDecoded!.sub),
+  });
+
+  useEffect(() => {
+    if (!isLoading && !isError) {
+      setUser(data!);
+    }
+  }, [data]);
+
+  return (
+    <main className="h-screen w-screen flex flex-col items-center p-2 gap-4">
+      <Header />
+      {children}
+    </main>
+  );
 }
